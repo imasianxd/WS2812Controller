3131fed4b4dafd3491567087bb6a2d305c63c7b2
jia <this>
2019-03-02 00:22:30 -0800

    added fixed to tab

--- animations/fixed.js
+++ animations/fixed.js
@@ -14,7 +14,6 @@
 	function fixed() {
 		this.fixedLighting = function(args, strip){
 			console.log("Starting fixed Color");
-			strip.Stop();
 			
 			FixedColor1 = parseInt("0x" + args.Color1);
 			Brightness = parseInt(args.Brightness);
@@ -25,17 +24,6 @@
 			strip.Mode = name + "fixed";
 			
 			console.log("started fixed mode");
-		};
-
-		this.FadeSpeed = function(args, strip) {
-			var val = parseInt(args.speed);
-	        var mappedVal = common.map_range(val, 0, 100, 10, 1);
-	        if (typeof mappedVal === "number") {
-				FadeSpeed = mappedVal;
-				console.log("Updated fade speed: " + FadeSpeed);
-	        } else {
-	            FadeSpeed = 1000 / 30;
-	        }
 		};
 	}
 
