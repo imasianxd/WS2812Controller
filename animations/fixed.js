340e77aaa66cafb46ce517cea2b3bb54406e117d
jia <this>
2019-03-21 18:33:00 -0700

    it doesnt like the new function? lets try rebuilding

--- animations/fixed.js
+++ animations/fixed.js
@@ -28,22 +28,6 @@
 			setTimeout(function () { strip.Render(); }, 1000);
 
 		};
-		
-		this.fixedLighting2 = function(args, strip) {
-		
-			console.log("turned on fixed lighting on pulse mode")
-			
-			//parse the args
-			FixedColor1 = parseInt("0x" + args.Color1);
-			Brightness = parseInt(args.Brightness);
-			
-			// init the led
-		    strip.SetStripColor(FixedColor1);
-			strip.SetBrightness(Brightness);
-			strip.Mode = name + "fixed";
-			strip.Render();
-			//render the strip continuously
-    };
 
 	}
 	
